0070ac5f951901e267604f8b249df6a6f0110dbe
dzulic420 <dzulic@unipu.hr>
2020-05-29 14:16:07 +0100

    search

---
+++ src/test/search.spec.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var chai = require("chai");
+var expect = chai.expect;
+var should = chai.should;
+const suggestionTool = require("../suggestions");
+
+describe('Pretrazivanje', function() {
+    it('Searching', function () {
+        suggestionTool();
+    });
+})
